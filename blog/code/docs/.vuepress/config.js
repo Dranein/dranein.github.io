1bd06574ca13e0507bcde0db869233df75918016
lijinyuanan_sz <lijinyuan@sz.baidu.com>
2020-01-02 21:38:10 +0800

    blog

--- blog/code/docs/.vuepress/config.js
+++ blog/code/docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     base: '/blog/',
-    title: '菜头丸首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+    title: '菜头丸的首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
     description: '菜头丸的前端学习记录', // meta 中的描述文字，用于SEO
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
